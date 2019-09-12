6cdf64b80f6bc62d2558f0fe623a250db438c4f9
Pranks-PJ <Pranks-PJ@.github.com>
2019-09-12 11:23:20 +0530

    Player Script
    Player Script added
    Main Building added

---
+++ Game/Assets/Our Assets/Scripts/move.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Input.GetAxis("Horizontal") * speed, 0f, 0f);
+    }
+}

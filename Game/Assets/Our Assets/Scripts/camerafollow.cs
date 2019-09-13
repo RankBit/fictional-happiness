9c116845a40501c60c5cb65b8a24a8558b8f819c
JazyyBlackX <JazyyBlackX@users.noreply.github.com>
2019-09-13 18:10:35 +0530

    0.0.4
    1. Changes to zoom Script

--- Game/Assets/Our Assets/Scripts/camerafollow.cs
+++ Game/Assets/Our Assets/Scripts/camerafollow.cs
@@ -6,6 +6,7 @@ public class camerafollow : MonoBehaviour
 {
     public Transform player;
     public float smooth = 9f;
+    public zoom a;
     public Vector3 offset = new Vector3(0f,2f,-10f);
     
     void Start()
@@ -17,6 +18,7 @@ public class camerafollow : MonoBehaviour
     
     void LateUpdate()
     {
+        
         Vector3 pos = player.position + offset;
         Vector3 smoothpos = Vector3.Lerp(transform.position, pos, smooth * Time.deltaTime);
         transform.position = smoothpos ;
---
+++ Game/Assets/Our Assets/Scripts/zoom.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class zoom : MonoBehaviour
+{
+    private Camera cam;
+    private camerafollow c;
+    private float zoomin;
+    int triggercount = 0;
+
+   /* private void OnTriggerStay(Collider other)
+    {
+        triggercount++;
+        cam = Camera.main; 
+        cam.fieldOfView = 37;
+        Debug.Log("Trigger");
+    }*/
+    private void OnTriggerEnter(Collider other)
+    {
+        c = FindObjectOfType<camerafollow>();
+        c.offset = new Vector3(0f, 1f, -5f);
+
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        c = FindObjectOfType<camerafollow>();
+        c.offset = new Vector3(0f, 2f, -10f);
+    }
+    /*private void OnTriggerEnter(Collider other)
+    {
+        triggercount++;
+        if (triggercount % 2 != 0)
+        {
+            cam = Camera.main;
+            cam.fieldOfView -= 30;
+        }
+        else
+        {
+            cam.fieldOfView = 57;
+        }
+        Debug.Log(triggercount);
+    }*/
+
+}

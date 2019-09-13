9ddbc1a73f51c34778c8fabdc713c283c7f27b3b
deptster <deepantripathi0000@gmail.com>
2019-09-14 03:22:29 +0530

    Update 0.0.5
    1. Default character with animation (fix)
    2. Added Jeremy Assets
    3. Replaced broken objects
    4. Script comments

---
+++ Game/Assets/Our Assets/Scripts/Player.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //movement
+    [SerializeField]float movespeed = 10f;
+    //removing SerializeField as it's not public by default
+
+    //public float movespeed = 10f;
+
+    //animation
+    public Animator anime;
+    private float inputH;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        //movement
+        movement(movespeed);
+
+        //animation
+        inputH = Input.GetAxis("Horizontal");
+        anime.SetFloat("inputH",inputH);
+
+    }
+
+    //For Smooth movement
+    public void movement(float move)
+    {
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * move;
+        var newXPos = transform.position.x + deltaX;
+        transform.position = new Vector3(newXPos, transform.position.y,transform.position.z);
+
+    }
+    
+   
+}
--- Game/Assets/Our Assets/Scripts/zoom.cs
+++ Game/Assets/Our Assets/Scripts/zoom.cs
@@ -9,17 +9,33 @@ public class zoom : MonoBehaviour
     private float zoomin;
     int triggercount = 0;
 
-   /* private void OnTriggerStay(Collider other)
-    {
-        triggercount++;
-        cam = Camera.main; 
-        cam.fieldOfView = 37;
-        Debug.Log("Trigger");
-    }*/
+
+
+    /////  Trying to bring camera closer by invoking player class in this class {failed}
+    //public Transform or GameObject player;
+    //private void Start()
+    //{
+    //    Player play = player.GetComponent<Player>();
+    //    play.movement(5);
+    //}
+
+
+    /* private void OnTriggerStay(Collider other)
+     {
+         triggercount++;
+         cam = Camera.main; 
+         cam.fieldOfView = 37;
+         Debug.Log("Trigger");
+     }*/
     private void OnTriggerEnter(Collider other)
     {
         c = FindObjectOfType<camerafollow>();
         c.offset = new Vector3(0f, 1f, -5f);
+
+
+        //{same as above}
+        //Player play = player.GetComponent<Player>();
+        //play.movement(5);
 
     }
     private void OnTriggerExit(Collider other)

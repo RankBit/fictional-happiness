15eb9490d56fc5c80bf95bf3fbabf05dcbe0d83a
JazyyBlackX <JazyyBlackX@users.noreply.github.com>
2019-09-14 13:53:08 +0530

    Update 0.0.6
    1. Added the jump feature
    
    Note - Collider for the player has to be changed.

--- Game/Assets/Our Assets/Scripts/Player.cs
+++ Game/Assets/Our Assets/Scripts/Player.cs
@@ -7,18 +7,33 @@ public class Player : MonoBehaviour
 {
     //movement
     [SerializeField]public float movespeed = 10f;
+    Rigidbody r;
+    public bool isGrounded;
 
     //animation
     public Animator anime;
     private float inputH;
+
+    private void Start()
+    {
+        r = GetComponent<Rigidbody>();
+    }
     void Update()
     {
         //movement
-        movement();
-
+        if (isGrounded)
+        {
+            movement();
+            inputH = Input.GetAxis("Horizontal");
+            anime.SetFloat("inputH", inputH);
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            r.AddForce(new Vector3(0, 5, 0), ForceMode.Impulse);
+            isGrounded = false;
+        }
         //animation
-        inputH = Input.GetAxis("Horizontal");
-        anime.SetFloat("inputH",inputH);
+        
     }
 
     //For Smooth movement
@@ -27,5 +42,13 @@ public class Player : MonoBehaviour
         var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * movespeed;
         var newXPos = transform.position.x + deltaX;
         transform.position = new Vector3(newXPos, transform.position.y,transform.position.z);
+    }
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == ("Ground") && isGrounded == false)
+        {
+            isGrounded = true;
+        }
+
     }
 }

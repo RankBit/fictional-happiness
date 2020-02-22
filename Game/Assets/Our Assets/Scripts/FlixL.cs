8be51fac9831dc7a45ea288479c4769064d4ee41
JazyyBlackX <JazyyBlackX@users.noreply.github.com>
2020-02-22 17:03:21 +0530

    Update 0.8
    - Script for Flickering Light added
    - Script attached to first two lamps

---
+++ Game/Assets/Our Assets/Scripts/FlixL.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlixL : MonoBehaviour
+{
+    Light testLight;
+	public float min;
+	public float max;
+	
+	void Start () {
+		testLight = GetComponent<Light>();
+		StartCoroutine(Flashing());
+	}
+	
+	IEnumerator Flashing ()
+	{
+		while (true)
+		{
+			yield return new WaitForSeconds(Random.Range(min,max));
+			testLight.enabled = ! testLight.enabled;
+
+		}
+	}
+}

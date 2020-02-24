8098c0ed83a71332356aadbef6a5a1d7324a549e
deptster <deepantripathi0000@gmail.com>
2020-02-24 13:21:04 +0530

    Revert "HDRP Update 0.0.9"

--- Game/Assets/Our Assets/Scripts/MainMenu.cs
+++ Game/Assets/Our Assets/Scripts/MainMenu.cs
@@ -51,7 +51,7 @@ public class MainMenu : MonoBehaviour
 
     public void PlayGame()
     {
-        SceneManager.LoadScene("Scene1");
+        SceneManager.LoadScene("Main");
     }
 
     public void QuitGame()
